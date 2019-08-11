cb2e03506f14094d84569427f0890008c709745a
BrunoSan123 <44749250+BrunoSan123@users.noreply.github.com>
2019-08-11 07:32:06 -0300

    Update boteng.js
    Atualizações feitas, e recursividade em desenvolvimento, para testes, elimine os comentários deixados no switch

--- src/mods/boteng.js
+++ src/mods/boteng.js
@@ -1,4 +1,5 @@
 
+
 //server-------------------
 const restify = require('restify');
 const builder = require('botbuilder');
@@ -46,7 +47,37 @@ bot.dialog('/', [
 
 
 
-
+/*bot.dialog('/pergunta', [
+   session => {
+       builder.Prompts.text(session, 'Escolha uma das opções');
+   },
+    (session, results) => {
+        let escolha = results.response;
+        
+        switch(escolha) {
+            case 'a':
+//                session.beginDialog('/pergunta_a');
+                session.endDialog('');
+                break;
+            case 'b':
+//                session.beginDialog('/pergunta_b');
+                session.endDialog('');
+                break;
+            case 'c':
+//                session.beginDialog('/pergunta_c');
+                session.endDialog('');
+                break;
+            case 'd':
+//                session.beginDialog('/pergunta_d');
+                session.endDialog('');
+                break;
+            case 'e':
+//                session.beginDialog('/pergunta_e');
+                session.endDialog('');
+                break;
+        }
+    }
+]);*/
 
 
         
@@ -61,10 +92,9 @@ bot.dialog('/perguntaNBR', [
     (session, results) => {
         
         let paramNBR = results.response;
-        if(paramNBR == 'vestiário'){
+        if(paramNBR == 'Quantos lavatórios são necessários no canteiro?'){
         
-        session.endDialog(`A NBR para ${paramNBR}, diz:
-A NBr 12284 diz que  no caso de vestiarios, estes devem estar o mais próximo da entrada da obra e das intalações sanitárias; com seu acesso protegido das intempéries.`);}else{
+        session.endDialog(`É necessário 1 (um) conjunto para cada grupo de 20 (vinte) trabalhadores ou fração (NR 18.2.4).`);}else{
             
             session.send('SEJA MAIS ESPECIFICO');
             
@@ -88,8 +118,8 @@ bot.dialog('/perguntaNBR2', [
     (session, results) => {
         
         let NBR2 = results.response;
-        if(NBR2 == 'refeitório'){
-        session.endDialog(`segundo a NBR 12284; As áreas de vivência devem possuir refeitórios, indpendentemente do número de trabalhadores.`);}else{
+        if(NBR2 == 'Quantos chuveiros são necessários no canteiro?'){
+        session.endDialog(`É necessário 1 (uma) unidade para cada grupo de 10 (dez) trabalhadores ou fração (NR 18.2.4).`);}else{
             
             session.send('Digite a referencia certa');
         }
@@ -111,8 +141,15 @@ session => {
     (session, results) => {
         
         let NBR3 = results.response;
-        if(NBR3 == 'chumaço'){
-        session.endDialog(`Segundo a NBR 5628 para ${NBR3}; O chumaço deve ter área de (100x100) mm na superficie, e ter uma espessura de cerca de 20 mm, pesande entre 3 a 4 g, e ser fixado por meio de grampos e arames de aço a um bastidor de 100mm de lado,também de arame de aço de lado, também de arame de aço com cerca de 1m de diametro.O batedor é fixado em uma alça também de arame de aço com comprimento de, aproximadamente 750mm.`);}else{
+        if(NBR3 == 'Como deve ser a instalação dos lavatórios?'){
+        session.endDialog(`A instalação dos lavatórios deve seguir as recomendações de acordo com a Norma Regulamentadora NR 18 (18.4.2.5.1):\n
+•	Possuir torneira de metal ou de plástico;\n 
+•	Ficar a uma altura de 0,90m (noventa centímetros);\n 
+•	Ser ligados diretamente à rede de esgoto, quando houver;\n 
+•	Ter revestimento interno de material liso, impermeável e lavável;\n 
+•	Ter espaçamento mínimo entre as torneiras de 0,60m (sessenta centímetros), quando coletivos\n
+•	Dispor de recipiente para coleta de papéis usados.
+`);}else{
             
             session.send('Digite a referencia certa!!!!');
         }
@@ -134,9 +171,17 @@ bot.dialog('/perguntaNBR4', [
     (session,results) => {
         
         let NBR4 = results.response;
-        if(NBR4 == 'Fogo-livre'){
+        if(NBR4 == 'Como deve ser feito a armazenagem dos materiais em geral?'){
         
-        session.endDialog(`Segundo a NBR 14432 sobre ${NBR4}; Diz que os elementos estruturais podem ser construidos sem a resistência ao fogo exigida por essa norma, desde que se desmonstrem que estejam livres da ação do incendio.`);}else{
+        session.endDialog(`• Fica estabelecida a distância máxima de 60,00m (sessenta metros) para o transporte manual de um saco (NR 11.2.2);\n
+• As pilhas de sacos, nos armazéns, terão a altura máxima correspondente a 30 (trinta) fiadas de sacos quando for usado processo mecanizado de empilhamento (11.2.5);\n
+• A altura máxima das pilhas de sacos será correspondente a 20 (vinte) fiadas quando for usado processo manual de empilhamento (NR 11.2.6);\n
+• Material empilhado deverá ficar afastado das estruturas laterais do prédio a uma distância de pelo menos 0,50m (cinquenta centímetros) (NR 11.3.3).\n
+
+Obs 1: O empilhamento máximo deve atentar ainda para as recomendações do fabricante;
+
+Obs 2: Os sacos de cimento ser dispostos em pallets e protegidos da umidade e intempéries;
+.`);}else{
             
             session.send('Essa não é a referencia.');
         }
@@ -155,9 +200,9 @@ bot.dialog('/perguntaNBR5', [
     
     (session,results) => {
         let NBR5 =results.response;
-        if(NBR5 == 'TRRF'){
+        if(NBR5 == 'É necessário ambulatório em um canteiro de obras?'){
         
-        session.endDialog(`Segundo a ${NBR5} referente á NBR 14432 diz que: Para ocupação/uso residencial(grupo A), Profundidade do solo como classe S2 tem h0> 10m e 90m; para classes S1 tem h0 <= 10m e tempo = a 30(60) min, levando-se em consideração a profundidade do subsolo.`);}else{
+        session.endDialog(`O canteiro de obras deve dispor de ambulatório, quando se tratar de frentes de trabalho com 50 (cinquenta) ou mais trabalhadores (NR 18.4.1)`);}else{
             
             session.send('ZZZZZZZZZZZZZ');
         }
